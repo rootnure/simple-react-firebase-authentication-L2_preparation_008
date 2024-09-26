62549c08462d56d538186d3586c72bfc47988527
rootnure <nur.diu.2791@gmail.com>
2024-09-26 20:36:01 +0600

    Project basic setup (react router, firebase)

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Main from './components/layout/Main';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
