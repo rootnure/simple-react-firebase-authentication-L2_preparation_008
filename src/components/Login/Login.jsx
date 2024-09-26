15bff300835cd06ddfa1f89df15d1b5c3906df8f
rootnure <nur.diu.2791@gmail.com>
2024-09-26 21:30:19 +0600

    Display logged in user info and signOut added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,47 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+
+    const [user, setUser] = useState(null);
 
     const auth = getAuth(app);
 
     const googleProvider = new GoogleAuthProvider();
 
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log('sign out successful');
+                setUser(null);
+            })
+            .catch(err => console.error(err))
+    }
 
     const handleGoogleSingIn = () => {
         signInWithPopup(auth, googleProvider)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
             })
             .catch(err => console.error(err))
     }
 
     return (
         <div>
-            <h2>Login</h2>
-            <button onClick={handleGoogleSingIn}>Login</button>
+            {user &&
+                <div>
+                    <h3>User: {user.displayName}</h3>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
+            {user ?
+                <button onClick={handleSignOut}>Sign Out</button> :
+                <button onClick={handleGoogleSingIn}>Google Login</button>
+            }
         </div>
     );
 };
